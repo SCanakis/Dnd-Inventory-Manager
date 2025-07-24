99c6ba7db79ec2e7145cd57d57996e120ffecee7
Sebastian Canakis <sac2693@g.rit.edu>
2025-07-24 16:25:26 -0500

    init-extensions, init-data

---
+++ db/init-extensions.sql
@@ -0,0 +1,4 @@
+-- Create extensions that your app needs
+-- JPA will handle all table creation and schema management
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
